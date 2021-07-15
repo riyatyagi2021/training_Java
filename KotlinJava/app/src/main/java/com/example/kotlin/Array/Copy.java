de40c2c4273e74e784a1db9b4f71a518249c340b
riyatyagi2021 <riya.mobcoder@gmail.com>
2021-07-15 16:56:15 +0530

    codes

---
+++ KotlinJava/app/src/main/java/com/example/kotlin/Array/Copy.java
@@ -0,0 +1,12 @@
+package com.example.kotlin.Array;
+
+public class Copy {
+    public static void main(String[] args) {
+        int arr1[]={1,2,3,4,5};
+        int arr2[]=new int[5];
+        for(int i=0;i<arr1.length;i++){
+            arr2[i]=arr1[i];
+        }
+        System.out.println(git);
+    }
+}
